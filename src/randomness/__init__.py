f0b41e20945154e9f832554cc804e1e615ee1bad
codylane <cody.lane@gmail.com>
2024-03-19 16:14:10 -0600

    tests

--- src/randomness/__init__.py
+++ src/randomness/__init__.py
@@ -2,3 +2,7 @@
 
 
 import random
+
+
+def random_generator(samples, size):
+    return [random.randrange(1, size + 1) for _ in range(0, size)]
--- tests/test_randomness.py
+++ tests/test_randomness.py
@@ -1,9 +1,21 @@
 # coding: utf-8
 
-from randomness import weighted_random_generator
+from randomness import random_generator
 
 
-def test_weighted_random_generator():
-    assert weighted_random_generator == -1
+def test_random_generator_generates_the_proper_size_of_items():
+    samples = [1, 2, 3]
+    size = 100
 
-    assert False
+    results = random_generator(
+        samples=samples,
+        size=size,
+    )
+
+    results[-1] = 5
+    results_len = len(results)
+
+    assert results_len == size, f'Expected random generator to produduce size {size} but got {results_len}'  # noqa: E501
+
+    print()
+    print(results)
