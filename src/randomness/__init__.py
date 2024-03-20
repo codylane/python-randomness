05eee5a484a3cdbfa72e0f931b3c1aebd07e51bd
codylane <cody.lane@gmail.com>
2024-03-19 21:24:05 -0600

    WIP

--- src/randomness/__init__.py
+++ src/randomness/__init__.py
@@ -4,5 +4,26 @@
 import random
 
 
-def random_generator(samples, size):
-    return [random.randrange(1, size + 1) for _ in range(0, size)]
+def random_generator(size, start=None, end=None, **kwargs):
+    '''Generates a list of random numbers
+
+    params:
+        size (int)  - Required
+                    - The number of total items the generator should perform
+                      calculations.
+        start (int) - Optional - The start of the range. Defaults to `1`
+        end (int)   - Optional -The end of the range. Defaults to `size + 1`
+        kwargs (dict): To be passed to random.randrange
+
+    returns:
+        (list) - Of random numbers
+
+    '''
+    start = start or 1
+    size += 1
+    end = end or size
+
+    return [
+        random.randrange(start, end + 1, **kwargs)
+        for _ in range(0, size - 1)
+    ]
--- tests/test_randomness.py
+++ tests/test_randomness.py
@@ -4,18 +4,27 @@ from randomness import random_generator
 
 
 def test_random_generator_generates_the_proper_size_of_items():
-    samples = [1, 2, 3]
-    size = 100
+    size = 10000
+    start = 1
+    end = 3
 
     results = random_generator(
-        samples=samples,
         size=size,
+        start=start,
+        end=end,
     )
 
-    results[-1] = 5
     results_len = len(results)
 
     assert results_len == size, f'Expected random generator to produduce size {size} but got {results_len}'  # noqa: E501
 
-    print()
-    print(results)
+    expected_range = {
+        1: 0,
+        2: 1,
+        3: 2,
+    }
+
+    for i, v in enumerate(results):
+        i = i % end + 1
+        expected_range[i] += 1
+        assert v in expected_range
