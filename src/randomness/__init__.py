10e71eef85ebf830cce098d8ad1a72dff2766f2c
codylane <cody.lane@gmail.com>
2024-03-28 10:08:04 -0600

    optimization to make things move a little faster

--- src/randomness/__init__.py
+++ src/randomness/__init__.py
@@ -61,7 +61,7 @@ def weighted_random_generator(weights, size, start=None, end=None, **kwargs):  #
 
     while len(results) < size:
         random_iterator = random_generator(
-                size=end,
+                size=size,
                 start=start,
                 end=end,
                 **kwargs,
